f101771e9efb872e5049e28b5c0dbb8a12f4090c
Dan Dimond <dtdimond@gmail.com>
2014-06-30 22:52:37 +1000

    Trying to debug non-working JS

--- public/application.js
+++ public/application.js
@@ -1,14 +1,20 @@
 $(document).ready( function() {
 
   $(document).on('click', '#hit input', function() {
-    $.ajax({
-      type: 'POST',
-      url: '/game/hit/player'
-    }).done(function(msg){
-      ('#game_template').replaceWith(msg);
-    });
+//    $.ajax({
+//      type: 'POST',
+//      url: '/game/hit/player'
+//    }).done(function(msg){
+//      ('#game_template').replaceWith(msg);
+//    });
+    alert('test');
     return false;
   });
+
+  $('#stay').click(function(){
+    alert("test");
+    return false;
+  }
 
   $('.dropdown-toggle').dropdown();
 });
